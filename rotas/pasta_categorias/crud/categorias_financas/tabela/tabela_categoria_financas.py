ccbbc464c5896f0eed34d944360b5b6f0b03436e
vitorhugoarcanjo <dias.vitorhd@gmail.com>
2026-05-24 20:23:35 -0400

    Subindo tabela nova  -- categorias financas

--- rotas/pasta_categorias/crud/categorias_financas/tabela/tabela_categoria_financas.py
+++ rotas/pasta_categorias/crud/categorias_financas/tabela/tabela_categoria_financas.py
@@ -5,11 +5,10 @@ def tabela_categorias_financas(cursor):
         cursor.execute("""
         CREATE TABLE IF NOT EXISTS categorias_financas (
         id INTEGER PRIMARY KEY,
-        user_id INTEGER,
-        nome TEXT,
-        tipo TEXT,               
+        user_id INTEGER,  -- ID DO USUARIO
+        nome TEXT, -- NOME DA CATEGORIA
         cor TEXT,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- DATA DE CRIAÇÃO
         FOREIGN KEY (user_id) REFERENCES cadastre_se(id) ON DELETE CASCADE
         )
     """)
