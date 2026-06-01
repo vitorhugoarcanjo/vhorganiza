e579d3a4edc19c3458ea4c5c2f29193c5bdb8932
vitorhugoarcanjo <dias.vitorhd@gmail.com>
2026-05-31 23:07:49 -0400

    Ajustando versionamento manual do css 1.0.2 com força 12.0

--- app.py
+++ app.py
@@ -23,22 +23,29 @@ app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 logica_imports(app) # IMPORTAÇÃO DOS BLUEPRINTS
 app.secret_key = os.getenv('SECRET_KEY') # CHAVE SECRETA
 
+
+# CONFIG GLOBAL
+app.config['VERSAO_SISTEMA'] = '1.0.3'
+
 @app.context_processor
 def inject_global_contexts():
+
     def static_v(filename):
         file_path = os.path.join(app.static_folder, filename)
 
         try:
             with open(file_path, 'rb') as f:
-                version = hashlib.md5(f.read()).hexdigest()[:10]
+                hash_file = hashlib.md5(f.read()).hexdigest()[:6]
         except:
-            version = '1'
+            hash_file = '1'
+
+        version = f"{hash_file}_{app.config['VERSAO_SISTEMA']}"
 
         return url_for('static', filename=filename, v=version)
     
     return {
         'static_v': static_v,
-        'versao_sistema': '1.0.0'
+        'versao_sistema': app.config['VERSAO_SISTEMA']
     }
 
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=240) # SESSÃO DE LOGIN 60    
